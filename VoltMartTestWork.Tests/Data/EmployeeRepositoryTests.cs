0b753bde66a28c90c7ea39b9481b46e2e759a669
Lks <nbvf01@list.ru>
2025-04-05 17:19:32 +0300

    добавил тест с mock в Data tests

---
+++ VoltMartTestWork.Tests/Data/EmployeeRepositoryTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Moq;
+using System.Reflection;
+using VoltMartTestWork.Core.Models;
+using VoltMartTestWork.Data;
+using VoltMartTestWork.Data.Repositories;
+using Xunit;
+
+namespace VoltMartTestWork.Tests.Data
+{
+    public class EmployeeRepositoryTests
+    {
+        private Mock<AppDbContext> _dbContextMock;
+        private EmployeeRepository _employeeRepository;
+
+        public EmployeeRepositoryTests()
+        {
+            Setup();
+        }
+        [Fact]
+        public async Task AddAsync_ValidEmployee_ReturnsAddedEmployee()
+        {
+            //Arrange
+            var newEmployee = GetTestEmployeeObject();
+
+            var employeeDbSetMock = new Mock<DbSet<Employee>>();
+            //вернёт db.Set<Employee>()
+            _dbContextMock.Setup(db => db.Set<Employee>())
+                .Returns(employeeDbSetMock.Object);
+            // вернёт mewEmployee обьект
+            _dbContextMock.Setup(dbSet => dbSet.AddAsync(newEmployee, default))
+                .ReturnsAsync((EntityEntry<Employee>)null);
+            //Act
+            var result = await _employeeRepository.CreateEntity(newEmployee);
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(newEmployee, result);
+        }
+
+
+
+        //sub methods
+        public void Setup()
+        {
+            _dbContextMock = new Mock<AppDbContext>(new DbContextOptions<AppDbContext>());
+            _employeeRepository = new EmployeeRepository(_dbContextMock.Object);
+        }
+
+        private Employee GetTestEmployeeObject()
+        {
+            Employee employee = new Employee();
+            employee.Firstname = "test";
+            employee.Lastname = "test";
+            employee.Isworking = true;
+            employee.Birthday = DateOnly.FromDateTime(DateTime.Today);
+            return employee;
+        }
+    }
+}
