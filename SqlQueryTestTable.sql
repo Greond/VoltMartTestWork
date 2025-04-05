3fe9dadd91258badeaa0282a7f7203bc1206587d
Lks <nbvf01@list.ru>
2025-04-05 03:38:49 +0300

    Sql Queary for Test Table

---
+++ SqlQueryTestTable.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE employee(
+    id BIGINT PRIMARY KEY,
+    firstname VARCHAR(255) ,
+    lastname VARCHAR(255) ,
+    midlename VARCHAR(255) NULL,
+    phone VARCHAR(255) NULL,
+    email VARCHAR(255) NULL,
+    birthday DATE NULL,
+    createat DATE NOT NULL,
+    updateat DATE NULL,
+    isworking BOOLEAN NULL,
+    ismarried BOOLEAN NULL,
+    nowplaceliving TEXT NULL,
+    hiredate DATE NULL
+);
